8ab752df253001131716ad2ed56a6d4ba5e0ad5e
Afnan Shehta <afnanyaseen85@gmail.com>
2026-07-31 17:03:27 +0300

    update

--- Homepage.js
+++ Homepage.js
@@ -1,7 +1,9 @@
- let buttons=document.querySelectorAll(".drop-btn");
+//عشان الدروب داون بتاعة اللغة و العملة نضغط عليها و تبين القايمة
+let buttons=document.querySelectorAll(".drop-btn");
 buttons.forEach(btn=>{
     btn.onclick=function(e){
         e.stopPropagation();
+
         let menu=this.nextElementSibling;
         document.querySelectorAll(".drop-menu").forEach(item=>{
             if(item!=menu){
@@ -15,15 +17,21 @@ window.onclick=function(){
     document.querySelectorAll(".drop-menu").forEach(item=>{
         item.classList.remove("show");
     });
-}
+};
+
+//قايمة تغيير الغة
 function changeLang(name,element){
     document.querySelector("#langBtn span").innerText=name;
-    document.querySelectorAll(".dropdown")[0]
-    .querySelectorAll(".drop-item")
-    .forEach(item=>item.classList.remove("active"));
+    document
+            .querySelectorAll(".dropdown")[0]
+            .querySelectorAll(".drop-item")
+            .forEach(item=>item.classList.remove("active"));
+
     element.classList.add("active");
     element.parentElement.classList.remove("show");
 }
+
+//قايمة تغيير العملة 
 function changeCurrency(name,element){
     document.querySelector("#currencyBtn span").innerText=name;
     document.querySelectorAll(".dropdown")[1]
@@ -32,8 +40,11 @@ function changeCurrency(name,element){
     element.classList.add("active");
     element.parentElement.classList.remove("show");
 }
+
+//عشان لما نضغط على زرار all category يستجيب
 let categoryBtn = document.getElementById("categoryBtn");
 let megaMenu = document.getElementById("megaMenu");
+
 categoryBtn.onclick = function(e){
     e.stopPropagation();
     megaMenu.classList.toggle("show");
@@ -42,28 +53,34 @@ document.addEventListener("click",function(){
     megaMenu.classList.remove("show");
 }); 
 
-
-
+//زرار القفل
 const closeBtn = document.querySelector(".close-btn");
-closeBtn.addEventListener("click", function () {
+closeBtn.addEventListener("click", function ()
+ {
     window.location.href = "https://www.google.com";
 });
+
+//التايمر
 const endDate = new Date("August 15, 2026 23:59:59").getTime();
 const timer = document.querySelector(".timer");
+
 function updateTimer() 
 {
     const now = new Date().getTime();
     const distance = endDate - now;
+
     if (distance <= 0) 
         {
         timer.innerHTML = "Expired";
         clearInterval(countdown);
         return;
     }
+
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
     timer.innerHTML = `${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
 }
 updateTimer();
